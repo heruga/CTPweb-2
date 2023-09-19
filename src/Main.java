54b3e797aeff45ca5c50928a32b64a28131368fc
heruga <olga4ernyakova@gmail.com>
2023-09-19 13:14:31 +0300

    hehe

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,8 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("как же хочеца пивасика, а не вот это все");
-<<<<<<< HEAD
-        System.out.println("Не просто пивасика, а водочки))");
-=======
-        System.out.println("Не просто пивасика, а водочки");
->>>>>>> origin/main
+
+        System.out.println("Не просто пивасика, а водочки)))");
+
     }
 }
